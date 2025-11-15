25e4021a6b63ffac12754436e107338dddb330a6
Fathima Naila <fathimanaila71@gmail.com>
2025-11-15 12:37:25 +0530

    login and register

--- hostel_app/urls.py
+++ hostel_app/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from hostel_app import views
+
 urlpatterns = [
-    
+    path('',views.login_user,name='login'),
+    path('register_student/',views.register_student,name='register_student'),
+    path('student_dashboard/',views.student_dashboard,name='student_dashboard'),
+    path('admin_dashboard/',views.admin_dashboard,name='admin_dashboard'),
+    path('logout/', views.logout_user, name='logout'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- hostel_app/views.py
+++ hostel_app/views.py
@@ -1,43 +1,40 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect
 from .models import StudentProfile
+from django.contrib.auth import logout
+
 
 def register_student(request):
     if request.method == 'POST':
-        roll_no = request.POST['roll_no']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        mobile_no = request.POST['mobile_no']
-        department = request.POST['department']
-        year_of_study = request.POST['year_of_study']
 
-        password = request.POST['password']
-        confirm_password = request.POST['confirm_password']
-
+        roll_no = request.POST.get('roll_no')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        mobile_no = request.POST.get('mobile_no')
+        department = request.POST.get('department')
+        year_of_study = request.POST.get('year_of_study')
         profile_photo = request.FILES.get('profile_photo')
 
-        # Password match validation
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
         if password != confirm_password:
             messages.error(request, "Passwords do not match")
-            return redirect('register')
+            return redirect('register_student')
 
-        # Username will be roll number (good for students)
         if User.objects.filter(username=roll_no).exists():
-            messages.error(request, "Roll number already registered")
-            return redirect('register')
+            messages.error(request, "Roll Number already registered")
+            return redirect('register_student')
 
-        # Create user (password hashed)
         user = User.objects.create(
             username=roll_no,
-            password=make_password(password)
+            password=make_password(password),
+            first_name=first_name,
+            last_name=last_name
         )
 
-        # Create profile
         StudentProfile.objects.create(
             user=user,
             roll_no=roll_no,
@@ -49,7 +46,41 @@ def register_student(request):
             profile_photo=profile_photo
         )
 
-        messages.success(request, "Registered successfully!")
+        messages.success(request, "Registration Successful!")
         return redirect('login')
 
     return render(request, 'register.html')
+
+
+
+
+from django.contrib.auth import authenticate, login
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            
+            if user.is_superuser:
+                return redirect('admin_dashboard')
+
+            return redirect('student_dashboard')
+
+        messages.error(request, "Invalid Username or Password")
+        return redirect('login')
+
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def student_dashboard(request):
+    return render(request,'student_dashboard.html')
+def admin_dashboard(request):
+    return render(request,'admin_dashboard.html')
